59b13966291034382998f509006baaabdf4e985f
vnavani <vinita.navani@gmail.com>
2023-10-27 10:08:56 -0700

    deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function AppNavbar() {
         <Container>
           <Navbar.Brand>Vinita Navani</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link href="/home">About</Nav.Link>
             <Nav.Link href="/projects">Projects</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
             <Nav.Link href="/resume">Resume</Nav.Link>
